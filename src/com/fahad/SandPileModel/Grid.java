c02d70ef2335ddc6cc772bc8c990706e42df5b5f
fzxt <fahadahmad41@hotmail.com>
2017-04-20 04:32:52 -0400

    Clean up

--- src/com/fahad/SandPileModel/Grid.java
+++ src/com/fahad/SandPileModel/Grid.java
@@ -1,20 +1,15 @@
 package com.fahad.SandPileModel;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.awt.Point;
 
 public class Grid {
-
     private Site[][] sites;
-    private int n;
     private int numAvalanches;
-    private Map<Integer, Integer> sizeCountMap;
+    private int n;
 
     public Grid(int n) {
         assert n > 0;
         this.n = n;
-        numAvalanches = 0;
-        sizeCountMap = new TreeMap<>();
         sites = new Site[n][n];
         initializeWithEmptySites();
     }
@@ -29,7 +24,7 @@ public class Grid {
     public void printSites() {
         for (int i = 0; i < sites.length; i++) {
             for (int j = 0; j < sites[i].length; j++) {
-                System.out.print(sites[i][j].toString() + " ");
+                     System.out.print(sites[i][j].toString() + " ");
             }
             System.out.println();
         }
@@ -39,15 +34,39 @@ public class Grid {
         return numAvalanches;
     }
 
-    private void incrementAvalancheCount() {
-        numAvalanches++;
+    /**
+     * Method used to reset the total avalanche count. Used at every time step.
+     */
+    void resetAvalancheCount() {
+        this.numAvalanches = 0;
     }
+
+    /**
+     * Recursive algorithm to topple when past a certain threshold
+     * If the threshold is passed, then we topple the site then recursively check its neighbours for any further topples.
+     * @param s             Site to topple
+     * @param threshold     Threshold to pass for topple
+     */
+    public void toppleAtThreshold(Site s, int threshold) {
+        if (s.getCurrentState() > threshold) {
+            topple(s);
+            for (Point neighbouringPoint : Utility.getValidNeighbourPoints(s, n-1)) {
+                // If the neighboring point is actually on the grid, we recursively check if it toppled
+                toppleAtThreshold(getSiteAtPoint(neighbouringPoint), threshold);
+            }
+        }
+    }
+
+    public Site getSiteAtPoint(Point point) {
+        return sites[point.x][point.y];
+    }
+
 
     /**
      * This will topple according to the model presented in Abelian Sandpile Model
      * @param site      The site one is trying to topple
      */
-    public void topple(Site site) {
+    private void topple(Site site) {
         incrementAvalancheCount();
 
         site.setState(site.currentState - 4);
@@ -57,38 +76,13 @@ public class Grid {
         }
     }
 
-    public void toppleAtThreshold(Site s, int threshold) {
-        if (s.getCurrentState() > threshold) {
-            topple(s);
-            incrementCountMap(getNumAvalanches());
-            for (Point neighbouringPoint : Utility.getValidNeighbourPoints(s, n-1)) {
-                // If the neighboring point is actually on the grid, we recursively check if it toppled
-                toppleAtThreshold(getSiteAtPoint(neighbouringPoint), threshold);
-            }
-        }
-
-        incrementCountMap(0);
+    /**
+     * Method to increment the total avalanche count
+     */
+    private void incrementAvalancheCount() {
+        numAvalanches++;
     }
 
-    public void resetAvalancheCount() {
-        this.numAvalanches = 0;
-    }
-
-    public Map<Integer, Integer> getSizeCountMap() {
-        return sizeCountMap;
-    }
-
-    public Site getSiteAtPoint(Point point) {
-        return sites[point.x][point.y];
-    }
-
-    private void incrementCountMap(int avalancheSize) {
-        if (sizeCountMap.containsKey(avalancheSize)) {
-            sizeCountMap.put(avalancheSize, sizeCountMap.get(avalancheSize) + 1);
-        } else {
-            sizeCountMap.put(avalancheSize, 1);
-        }
-    }
 
     private void initializeWithEmptySites() {
         for (int i = 0; i < n; i++) {
@@ -97,5 +91,4 @@ public class Grid {
             }
         }
     }
-
 }
--- src/com/fahad/SandPileModel/Main.java
+++ src/com/fahad/SandPileModel/Main.java
@@ -1,11 +1,9 @@
 package com.fahad.SandPileModel;
 
+import java.awt.Point;
 import java.util.Map;
 import java.util.Random;
-
-/**
- * @author Fahad Ahmad
- */
+import java.util.TreeMap;
 
 /**
  * This program simulates the Abelian Sandpile Model, by Bak et al
@@ -16,14 +14,17 @@ import java.util.Random;
 
 public class Main {
 
+    private static final int N = 50;
+    private static final int T = 2000;
+    private static final int THRESHOLD = 3;
 
-    static final int N = 200;
-    static final int T = 80000;
-    static final int THRESHOLD = 3;
 
     public static void main(String[] args) {
-        Random random = new Random(1234);
+        Random random = new Random(12930);
         Grid grid = new Grid(N);
+        Map<Integer, Integer> sizeCountMap = new TreeMap<>();
+
+        System.out.printf("Running simulations with N: %d and T: %d\n\n", N, T);
 
         for (int t = 0; t < T; t++) {
             // Randomly select a site between 0 and N;
@@ -34,12 +35,26 @@ public class Main {
             Site s = grid.getSiteAtPoint(randomPoint);
             s.increment();
             grid.toppleAtThreshold(s, THRESHOLD);
+
+            int numAvalanches = grid.getNumAvalanches();
+
+            if (sizeCountMap.containsKey(numAvalanches)) {
+                // If the number of avalanches already exists in our map, increment the count
+                sizeCountMap.put(numAvalanches, sizeCountMap.get(numAvalanches) + 1);
+            } else {
+                // Else, initialize the count to 1
+                sizeCountMap.put(numAvalanches, 1);
+            }
+
+            // Reset the avalanche count for the next time step
             grid.resetAvalancheCount();
         }
 
-        for (Map.Entry<Integer, Integer> entry : grid.getSizeCountMap().entrySet()) {
-            System.out.printf("%f %f\n", Math.log(entry.getKey()), Math.log(entry.getValue()));
+        for (Map.Entry<Integer, Integer> entry : sizeCountMap.entrySet()) {
+            System.out.printf("%f %f\n", Math.log10(entry.getKey()), Math.log10(entry.getValue()));
         }
+
+        System.out.println("\nDone");
     }
 
 }
